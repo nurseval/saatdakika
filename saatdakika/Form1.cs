f1c866d3fdf187054d3c47684428f5869dd0d6a9
Fatma Nur Seval <104559152+nurseval@users.noreply.github.com>
2022-07-16 13:47:04 +0300

    Add files via upload

---
+++ saatdakika/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saatdakika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi, dakika, saat;
+             
+            sayi= Convert.ToInt32(textBox1.Text);
+
+            saat = sayi / 60;
+            label2.Text= saat.ToString();
+
+            dakika = sayi % 60;
+            label4.Text= dakika.ToString();   
+        }
+    }
+}
